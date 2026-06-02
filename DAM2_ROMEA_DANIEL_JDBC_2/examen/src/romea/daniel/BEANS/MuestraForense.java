d354d6bafb1f87ddec9adf1425f54dc9e31b03b3
Daniel Romea <a29324@svalero.com>
2026-06-02 11:21:26 +0200

    Add files via upload

--- DAM2_ROMEA_DANIEL_JDBC_2/examen/src/romea/daniel/BEANS/MuestraForense.java
+++ DAM2_ROMEA_DANIEL_JDBC_2/examen/src/romea/daniel/BEANS/MuestraForense.java
@@ -8,22 +8,96 @@ public class MuestraForense {
     private String fechaRecogida;
     private String estadoCustodia;
     private CentroForense centro;
+    private InformeForense informe;
 
+    public MuestraForense() {
+    }
 
-    public MuestraForense(int codigoCaso, String tipoMuestra, String fechaRecogida, String estadoCustodia, CentroForense centro) {
+    public MuestraForense(int codigoCaso, String tipoMuestra, String fechaRecogida, String estadoCustodia, CentroForense centro, InformeForense informe) {
         this.codigoCaso = codigoCaso;
         this.tipoMuestra = tipoMuestra;
         this.fechaRecogida = fechaRecogida;
         this.estadoCustodia = estadoCustodia;
         this.centro = centro;
+        this.informe = informe;
     }
 
-    public MuestraForense(int id, int codigoCaso, String tipoMuestra, String fechaRecogida, String estadoCustodia, CentroForense centro) {
+    public MuestraForense(int id, int codigoCaso, String tipoMuestra, String fechaRecogida, String estadoCustodia, CentroForense centro, InformeForense informe) {
         this.id = id;
         this.codigoCaso = codigoCaso;
         this.tipoMuestra = tipoMuestra;
         this.fechaRecogida = fechaRecogida;
         this.estadoCustodia = estadoCustodia;
         this.centro = centro;
+        this.informe = informe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigoCaso() {
+        return codigoCaso;
+    }
+
+    public void setCodigoCaso(int codigoCaso) {
+        this.codigoCaso = codigoCaso;
+    }
+
+    public String getTipoMuestra() {
+        return tipoMuestra;
+    }
+
+    public void setTipoMuestra(String tipoMuestra) {
+        this.tipoMuestra = tipoMuestra;
+    }
+
+    public String getFechaRecogida() {
+        return fechaRecogida;
+    }
+
+    public void setFechaRecogida(String fechaRecogida) {
+        this.fechaRecogida = fechaRecogida;
+    }
+
+    public String getEstadoCustodia() {
+        return estadoCustodia;
+    }
+
+    public void setEstadoCustodia(String estadoCustodia) {
+        this.estadoCustodia = estadoCustodia;
+    }
+
+    public CentroForense getCentro() {
+        return centro;
+    }
+
+    public void setCentro(CentroForense centro) {
+        this.centro = centro;
+    }
+
+    public InformeForense getInforme() {
+        return informe;
+    }
+
+    public void setInforme(InformeForense informe) {
+        this.informe = informe;
+    }
+
+    @Override
+    public String toString() {
+        return "MuestraForense{" +
+                "id=" + id +
+                ", codigoCaso=" + codigoCaso +
+                ", tipoMuestra='" + tipoMuestra + '\'' +
+                ", fechaRecogida='" + fechaRecogida + '\'' +
+                ", estadoCustodia='" + estadoCustodia + '\'' +
+                ", centro=" + centro +
+                ", informe=" + informe +
+                '}';
     }
 }
