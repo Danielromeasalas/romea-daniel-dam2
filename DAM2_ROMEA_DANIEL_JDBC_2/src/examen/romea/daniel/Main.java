4084bc621b7190fd92a168e834c7024860dd994e
Daniel Romea <a29324@svalero.com>
2026-06-02 11:22:25 +0200

    Add files via upload

--- DAM2_ROMEA_DANIEL_JDBC_2/src/examen/romea/daniel/Main.java
+++ DAM2_ROMEA_DANIEL_JDBC_2/src/examen/romea/daniel/Main.java
@@ -1,5 +1,9 @@
 package src.examen.romea.daniel;
 
+import src.examen.romea.daniel.BEANS.CentroForense;
+import src.examen.romea.daniel.BEANS.InformeForense;
+import src.examen.romea.daniel.BEANS.MuestraForense;
+import src.examen.romea.daniel.DAO.MuestraForenseDAOImpl;
 import src.examen.romea.daniel.Motores.MotorFactory;
 import src.examen.romea.daniel.Motores.MotorSQL;
 
@@ -7,6 +11,17 @@ public class Main {
     public static void main(String[] args) {
 
 
+        MuestraForenseDAOImpl muestraDao = new MuestraForenseDAOImpl(MotorFactory.create(MotorFactory.POSTGRE));
+
+        CentroForense centro = new CentroForense(1, "NOMBRE", "PAIS", 10);
+        InformeForense informe = new InformeForense();
+        MuestraForense newMuestra = new MuestraForense(1213, "TipoMuestra", "01/02/2026", "EstadoCustodia", centro, informe);
+
+        muestraDao.add(newMuestra);
+        muestraDao.update(newMuestra);
+        System.out.println( muestraDao.find(2) );
+        System.out.println( muestraDao.findAll() );
+        System.out.println( muestraDao.findByCentro(centro) );
 
     }
 }
